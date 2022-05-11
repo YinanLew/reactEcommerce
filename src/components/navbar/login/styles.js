4ec1d2018cc679b9a9fa5a41f015eb6612dad10d
YinanLew <yinanlu0112@gmail.com>
2022-05-11 14:35:47 -0400

    updated login margin

--- src/components/navbar/login/styles.js
+++ src/components/navbar/login/styles.js
@@ -3,18 +3,20 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
     box: {
-        marginTop: '200px'
+        marginTop: '200px',
+        marginBottom: '210px'
     },
     title: {
         fontFamily: 'Montserrat, cursive, Arial '
     },
     container: {
         display: 'flex',
-        height: '100vh',
+        height: '150%',
         flexDirection: 'column',
         justifyContent: 'flex-start',
     },
     footer: {
-        marginTop: '200px'
+        height: '150%',
+        marginTop: 'auto'
     }
 }));
